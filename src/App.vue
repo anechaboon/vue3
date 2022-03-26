be5baf1518fdbad2e67b80901dfd5c93315482ed
zest19 <zest210793@gmail.com>
2022-03-26 14:23:33 +0700

    loop by v-for

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
 <h1>{{ title }}</h1>
+<ul>
+  <li v-for="task in tasks" :key="task.id">
+    {{ task.id }}. {{ task.name }}
+  </li>
+</ul>
 </template>
 
 <script>
@@ -7,7 +12,12 @@
 export default {
   data(){
     return{
-      title: 'My To-Do App'
+      title: 'My To-Do App',
+      tasks:[
+        { id: 1, name: 'Learn Vue', finished: false},
+        { id: 2, name: 'Build Vue', finished: false},
+        { id: 3, name: 'Build  o-Do App', finished: false},
+      ]
     }
   }
 }
